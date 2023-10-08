eda73cd8fd77b8bb5acbcde5b187b975a9bcffda
LalbaAnthony <anthony.lalba@limayrac.fr>
2023-10-08 21:30:32 +0200

    Side pannel

---
+++ js/header.js
@@ -0,0 +1,6 @@
+function toggleSidePanel() {
+    const sidePanel = document.getElementById("sidePanel");
+    // Si check si a la class 'open' 
+    // => si non lui ajouter
+    // => si oui lui retirer
+}
